ae0ade8a3affa88339b4e64c169afeb9c5485211
Emily G <efgodward@gmail.com>
2018-12-12 21:28:47 -0500

    rainbow background javascript

---
+++ class07/first-javascript/script.js
@@ -0,0 +1,44 @@
+console.log (10 * 10);
+
+
+let sum = 10*10
+console.log('product');
+
+let quotient = 100/10
+console.log('quotient');
+
+let subtract = 50-17
+console.log('subtract');
+
+let addition = 50+17
+console.log('sum');
+
+
+
+
+let first = 'emily';
+let last = 'godward';
+console.log(first + last);
+//alternative way...ES6 string templates//
+console.log(`Hello my name is ${first} ${last}!`);
+
+
+console.log(sum + sum * quotient + first);
+
+//to reassing, reassing your first name to another name (superhero)
+first = 'wonderwoman'
+//log out your first and last name again
+console.log(`Hello my name is ${first} ${last}!`);
+
+
+console.log(document.querySelector('h1'));
+
+//OR
+
+let hello = document.querySelector('h1');
+
+console.log(hello);
+
+hello.style.color =  'pink';
+hello.innerHTML = 'Hello world!';
+
