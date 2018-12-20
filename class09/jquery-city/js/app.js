1c7a7f5b6f188af96970c43ad4e2d42855b3762f
Emily G <efgodward@gmail.com>
2018-12-19 19:21:13 -0500

    $(this)

--- class09/jquery-city/js/app.js
+++ class09/jquery-city/js/app.js
@@ -1,23 +1,33 @@
 // 1. Create .click() handlers for each of the thumbnails: #first, #second, #third, #fourth
 // 2. Use .attr() to change the `src` attribute of #bigimage to correspond to image that was clicked
 
-let bigimage = $("#first").click(swapFirst);
-$("#second").click(swapSecond);
-$("#third").click(swapThird);
-$("#fourth").click(swapFourth);
+// let bigimage = $("#first").click(swapFirst);
+// $("#second").click(swapSecond);
+// $("#third").click(swapThird);
+// $("#fourth").click(swapFourth);
 
-function swapFirst() {
-  $("#bigimage").attr("src", "img/1.jpg");
-}
+// function swapFirst() {
+//   $("#bigimage").attr("src", "img/1.jpg");
+// }
 
-function swapSecond() {
-  $("#bigimage").attr("src", "img/2.jpg");
-}
+// function swapSecond() {
+//   $("#bigimage").attr("src", "img/2.jpg");
+// }
 
-function swapThird() {
-  $("#bigimage").attr("src", "img/3.jpg");
-}
+// function swapThird() {
+//   $("#bigimage").attr("src", "img/3.jpg");
+// }
 
-function swapFourth() {
-  $("#bigimage").attr("src", "img/4.jpg");
-}
+// function swapFourth() {
+//   $("#bigimage").attr("src", "img/4.jpg");
+// }
+
+
+
+$('.thumb').click(function() {
+  const imgURL = $(this).attr("src");
+  console.log(imgURL);
+  
+  $("#bigimage").attr("src", imgURL)
+})
+
