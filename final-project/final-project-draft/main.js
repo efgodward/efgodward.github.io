16aed8a4e093e04ec1dfac23d2006aabbc9c55f3
Emily G <efgodward@gmail.com>
2019-01-09 20:15:06 -0500

    class13

--- final-project/final-project-draft/main.js
+++ final-project/final-project-draft/main.js
@@ -2,4 +2,4 @@
 //google maps can be incorporated - we'll work on this later
 
 //collect some datat - list of restaurants: hoteltonight for restuarant reservations
-//do a sort of simulation - maybe something for Eleven Madision park, normally a 1 year wait! etc..
+//do a sort of simulation on an iphone screen- maybe something for Eleven Madision park, normally a 1 year wait! etc..
