702d59e4d113a00e66fd625b9124ea0d817fc902
Emily G <efgodward@gmail.com>
2019-01-16 19:37:54 -0500

    class 15

--- class14/animated-sticky-nav/js/app.js
+++ class14/animated-sticky-nav/js/app.js
@@ -1,2 +1,16 @@
 $(document).ready(function () {
+
+    $(window).scroll(function(){
+        let scrolltop = $(window).scrollTop();
+        console.log('scrollTop');
+    //how many pixels from normal top position you have scrolled down
+
+    if (scrolltop > 75) {
+        $('header').addClass('small-header')
+    }
+    else {
+        $('header').removeClass('small-header')
+    }
+        
+    })
 })
