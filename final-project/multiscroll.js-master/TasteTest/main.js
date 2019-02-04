cc16f7f6e8fcd1e7017c416f32dcdcc3940f70e9
Emily G <efgodward@gmail.com>
2019-02-04 11:32:47 -0500

    aesthetic updates

---
+++ final-project/multiscroll.js-master/TasteTest/main.js
@@ -0,0 +1,22 @@
+//look up services where you can submit an email form
+
+//collect some datat - list of restaurants: hoteltonight for restuarant reservations
+//do a sort of simulation on an iphone screen- maybe something for Eleven Madision park, normally a 1 year wait! etc..
+// document.querySelector('.submit-button').click = thankyouName;
+
+// function thankyouName () {
+//     let name = document.querySelector('name').value;
+
+//     let solution = (`Thanks! We'll be in touch, ${name}!`);
+
+//     document.querySelector(h3).innerHTML = solution;
+// }
+
+
+// function myFunction() {
+//     document.getElementById("frm1").submit();
+//   }
+
+// $("#frm1" ).click(function() {
+//     alert( "Thank you!");
+//   });
